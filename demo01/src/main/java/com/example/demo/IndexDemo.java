edc616d8b56426aceeeae5a5b79bafaacbf9a826
staff001 <951520363@qq.com>
2019-11-26 14:25:54 +0800

    创建demo01

---
+++ demo01/src/main/java/com/example/demo/IndexDemo.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IndexDemo {
+    public static final String abc = "sdss";
+
+
+
+    public static void main(String[] args) {
+
+
+
+        try {
+            ArrayList<String> list = new ArrayList<>();
+            for (String s : list) {
+
+            }
+
+            for (Iterator<String> iterator = list.iterator(); iterator.hasNext(); ) {
+                String next =  iterator.next();
+
+            }
+
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        ArrayList<String> lis2t = new ArrayList<>();
+
+        ArrayList<String> list3 = new ArrayList<>();
+
+
+        List list4;
+
+    }
+
+}
